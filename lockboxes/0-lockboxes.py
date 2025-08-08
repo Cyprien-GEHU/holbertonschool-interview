5bde1394b24ec4a585d99f316073360a267d9f5c
Cyprien <cypriengehu@example.com>
2025-08-08 12:07:50 +0200

    fix: correction pycodestyle pep8

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-''' method DPS determines if all box can be opened'''
+''' method DPS determines if all boxes can be opened'''
 
 
-def canUnlockAll(boxes) :
+def canUnlockAll(boxes):
     if not boxes or boxes[0] is None:
         return False
-    
-    lenght_boxes = len(boxes)
+
+    length_boxes = len(boxes)
     keys = boxes[0][:]
     open_box = {0}
 
@@ -14,13 +14,10 @@ def canUnlockAll(boxes) :
         if index not in open_box:
             open_box.add(index)
             keys.extend(boxes[index])
-        
 
     while keys:
         box = keys.pop()
-        if 0 <= box < lenght_boxes:
+        if 0 <= box < length_boxes:
             check_box(box)
 
-
-
-    return len(open_box) == lenght_boxes
+    return len(open_box) == length_boxes
