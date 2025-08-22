7c79bfa851a20a54886030c5441a7cdea99a868e
Cyprien <cypriengehu@example.com>
2025-08-22 09:58:31 +0200

    feat: insertation of a node

---
+++ heap_insert/0-binary_tree_node.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - fonction to point the node
+ * @parent: the parent of the node
+ * @value: the value of the node
+ * Return: NULL or the node
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+
+	if (node == NULL)
+		return (NULL);
+
+	node->parent = parent;
+	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
+	return (node);
+}
