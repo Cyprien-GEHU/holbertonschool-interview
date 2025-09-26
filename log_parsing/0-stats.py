ef10eed8f1079e8b2d662cb78d99e6e8c3dd10c4
Cyprien <cypriengehu@example.com>
2025-09-26 10:29:03 +0200

    feat: new version of log parsing

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,35 +1,61 @@
 #!/usr/bin/python3
-
 """ parse logs and return the number of error ans the size of the file"""
+
 import sys
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-total_size = 0
-count = 0
 
-try:
-    for line in sys.stdin:
-        parts = line.split()
-        if len(parts) < 9:
-            continue
+SC = [200, 401, 403, 404, 405, 500]
+
+
+def read_line(line):
+    """extract all data we need form the ligne"""
+    text = line.strip().split()
+    if not text:
+        return None, None
+
+    if len(text) >= 2:
         try:
-            code = int(parts[-2])
-            size = int(parts[-1])
-            total_size += size
-            if code in status_codes:
-                status_codes[code] += 1
-        except Exception:
-            continue
+            size_file = int(text[-1])
+        except (ValueError):
+            size_file = None
 
-        count += 1
-        if count % 10 == 0:
-            print("File size: {}".format(total_size))
-            for code in sorted(status_codes):
-                if status_codes[code]:
-                    print("{}: {}".format(code, status_codes[code]))
-except KeyboardInterrupt:
-    pass
-finally:
-    print("File size: {}".format(total_size))
-    for code in sorted(status_codes):
-        if status_codes[code]:
-            print("{}: {}".format(code, status_codes[code]))
+        try:
+            status_code = int(text[-2])
+        except ValueError:
+            status_code = None
+
+    else:
+        return None, None
+
+    return status_code, size_file
+
+
+def stats():
+    """the main function"""
+
+    n_line = 0
+    code = {}
+    total_size = 0
+
+    try:
+        for line in sys.stdin:
+            n_line += 1
+            status_code, size_file = read_line(line)
+
+            if size_file is not None:
+                total_size += size_file
+
+            if status_code is not None and status_code in SC:
+                code[status_code] = code.get(status_code, 0) + 1
+
+            if n_line % 10 == 0:
+                print(f'File size: {total_size}')
+                for status in sorted(SC):
+                    if status in code:
+                        print(f"{status}: {code[status]}")
+
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == "__main__":
+    stats()
