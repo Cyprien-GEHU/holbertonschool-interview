7413af3364d2d43c8f0d8925ae01ca342e5122e9
Cyprien <cypriengehu@example.com>
2025-10-03 10:40:56 +0200

    feat: projet utf-8 validate

---
+++ utf8_validation/0-validate_utf8.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Utf-8 validation
+"""
+
+
+def validUTF8(data):
+    """ the main function """
+    nBytes = 0
+
+    for nb in data:
+
+        b = format(nb, "08b")
+
+        if nBytes == 0:
+            if b.startswith('0'):
+                continue
+            elif b.startswith('110'):
+                nBytes = 1
+            elif b.startswith('1110'):
+                nBytes = 2
+            elif b.startswith('11110'):
+                nBytes = 3
+            else:
+                return False
+        else:
+            if not b.startswith('10'):
+                return False
+            nBytes -= 1
+
+    return nBytes == 0
