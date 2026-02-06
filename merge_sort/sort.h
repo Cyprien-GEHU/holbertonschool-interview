67d7b8f549981716fa44ea936619cc5d8c4f20d2
Cyprien <cypriengehu@example.com>
2026-02-06 14:20:45 +0100

    fix: correction sort.h

--- merge_sort/sort.h
+++ merge_sort/sort.h
@@ -7,9 +7,5 @@
 
 void merge_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
-static void merge_sort_recursive(int *array, int *aux, size_t left,
-		size_t right);
-static void merge_sort_recursive(int *array, int *aux, size_t left,
-		size_t right)
 
 #endif
