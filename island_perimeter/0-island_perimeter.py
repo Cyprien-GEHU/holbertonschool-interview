eee8ee4bfd1fb5a0f336bdb53978ab2e4a5f615d
Cyprien <cypriengehu@example.com>
2026-03-13 11:10:56 +0100

    fix: project interview - test new if for the range of col and row

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -9,16 +9,22 @@ def island_perimeter(grid):
     - grid : the representation of island
     return the perimeter of island
     """
-    count = 0 
-    for x in range(len(grid)):
-        for y in range(len(grid[x])):
+    count = 0
+    row = len(grid)
+    col = len(grid[0])
+    for x in range(row):
+        for y in range(col):
             if grid[x][y] == 1:
-                if grid[x - 1][y] == 0:
+                # top
+                if  x == 0 or grid[x - 1][y] == 0:
                     count += 1
-                if grid[x][y - 1] == 0:
+                #left
+                if  y == 0 or grid[x][y - 1] == 0:
                     count += 1
-                if grid[x][y + 1] == 0:
-                    count += 1 
-                if grid[x + 1][y] == 0:
+                #right
+                if y == col - 1 or grid[x][y + 1] == 0:
+                    count += 1
+                #bottom
+                if x == row -  1 or grid[x + 1][y] == 0:
                     count += 1
     return count
